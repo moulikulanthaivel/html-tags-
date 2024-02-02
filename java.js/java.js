53e1d24efd90ed870d711a358decef461736d358
moulikulanthaivel <moulikulanthaivel@gmail.com>
2024-02-02 01:40:58 -0800

    text.js

---
+++ java.js/java.js
@@ -0,0 +1,47 @@
+// var a = 200;
+// var b= 2000;
+// var c= "mouli";
+// var d= ;
+// e=true;
+//  f=false;
+
+
+// console.log(c+d)
+
+var a= 10;
+var b= 20.05;
+var c= 'mouli'
+var d=true;
+var e=false;
+var f= 'candy'
+var h=
+console.log(a*c)
+console.log(f+c)
+console.log(c+e)
+console.log(f*e)
+console.log(c+h)
+
+
+var a='2';
+var b=2;
+console.log(a<b);
+console.log( a+=1)
+
+
+
+var a = "moouli"
+var b =390
+
+if(a*b){
+    console.log("won a match")
+
+}
+else{
+console.log("lost the match")
+}  
+
+var a = "candy"
+var b = 0
+var c = 360
+
+b<c ? console.log("jigithom") :console.log("thoothutom")
